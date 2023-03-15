f7d7192ab5ce22009254c8070d69c15ff05deb25
Elysson Alves <elysson250102@gmail.com>
2023-03-14 22:03:26 -0300

    Cpp_studies

---
+++ Exercise_of_cpp/Sequential structure/Sum/sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sum, numberA, numberB;
+    cout << "Write a number A : ";
+    cin >> numberA;
+    cout << "Write a number B : ";
+    cin >> numberB;
+
+    sum = numberA + numberB;
+
+    cout << "The sumer of A and B is " << sum;
+}
